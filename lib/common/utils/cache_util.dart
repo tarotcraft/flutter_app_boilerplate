26945ffdd2ca10ab88ba2b656fb87ef16d95f4ec
realkimmy <upcwangying@126.com>
2021-10-16 13:32:58 +0800

    refactor: remove files

--- lib/common/utils/cache_util.dart
+++ lib/common/utils/cache_util.dart
@@ -1,20 +1,4 @@
 import 'dart:convert';
-import 'package:flutter_app_boilerplate/common/model/authorization.dart';
-import 'package:flutter_app_boilerplate/common/model/branch.dart';
-import 'package:flutter_app_boilerplate/common/model/content.dart';
-import 'package:flutter_app_boilerplate/common/model/contribution.dart';
-import 'package:flutter_app_boilerplate/common/model/contributor.dart';
-import 'package:flutter_app_boilerplate/common/model/issue.dart';
-import 'package:flutter_app_boilerplate/common/model/language_model.dart';
-import 'package:flutter_app_boilerplate/common/model/pull_request.dart';
-import 'package:flutter_app_boilerplate/common/model/readme.dart';
-import 'package:flutter_app_boilerplate/common/model/release.dart';
-import 'package:flutter_app_boilerplate/common/model/repository.dart';
-import 'package:flutter_app_boilerplate/common/model/repository_pulls_model.dart';
-import 'package:flutter_app_boilerplate/common/model/tree.dart';
-import 'package:flutter_app_boilerplate/common/model/trending_developer.dart';
-import 'package:flutter_app_boilerplate/common/model/trending_repository.dart';
-import 'package:flutter_app_boilerplate/common/model/user_detail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CacheUtil {
@@ -63,42 +47,6 @@ class CacheUtil {
 }
 
 dynamic myEncode(dynamic item) {
-  if (item is TrendingRepository) {
-    return item.toJson();
-  } else if (item is RepositoryBuildBy) {
-    return item.toJson();
-  } else if (item is TrendingDeveloper) {
-    return item.toJson();
-  } else if (item is PopularRepository) {
-    return item.toJson();
-  } else if (item is Language) {
-    return item.toJson();
-  } else if (item is Contribution) {
-    return item.toJson();
-  } else if (item is UserDetail) {
-    return item.toJson();
-  } else if (item is Repository) {
-    return item.toJson();
-  } else if (item is Issue) {
-    return item.toJson();
-  } else if (item is PullRequest) {
-    return item.toJson();
-  } else if (item is Contributor) {
-    return item.toJson();
-  } else if (item is Branch) {
-    return item.toJson();
-  } else if (item is Readme) {
-    return item.toJson();
-  } else if (item is RepositoryPullsModel) {
-    return item.toJson();
-  } else if (item is Tree) {
-    return item.toJson();
-  } else if (item is Content) {
-    return item.toJson();
-  } else if (item is Release) {
-    return item.toJson();
-  } else if (item is Authorization) {
-    return item.toJson();
-  }
+  // TODO: do something.
   return item;
 }
